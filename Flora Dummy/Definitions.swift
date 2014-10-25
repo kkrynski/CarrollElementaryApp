b5201dc8cd204e06dcf2727dfd55420b2471debc
Michael Schloss <michaelschloss@pal-nat184-068-005.itap.purdue.edu>
2014-10-25 16:35:15 -0400

    More Swifting

--- Flora Dummy/Definitions.swift
+++ Flora Dummy/Definitions.swift
@@ -6,6 +6,8 @@
 //  Copyright (c) 2014 SGSC. All rights reserved.
 //
 
+//  This Swift file will contain all functions that are found everywhere.  This reduces code load and centralizes any changed needed to be made.
+
 import UIKit
 
 class Definitions: NSObject
@@ -14,7 +16,7 @@ class Definitions: NSObject
     {
         let colorString = ((hexString as NSString).stringByReplacingOccurrencesOfString("#", withString: "")) as String
         
-        let alpha = 0.0
+        let alpha = 1.0
         let red = colorCompenentFrom(colorString, atStartIndex: 0, withLength: 2)
         let greenComponent = colorCompenentFrom(colorString, atStartIndex: 2, withLength: 2)
         let blue = colorCompenentFrom(colorString, atStartIndex: 4, withLength: 2)
@@ -22,14 +24,22 @@ class Definitions: NSObject
         return UIColor(red: CGFloat(red), green: CGFloat(greenComponent), blue: CGFloat(blue), alpha: CGFloat(alpha))
     }
     
+    class func outlineTextInLabel(label : UILabel)
+    {
+        label.layer.shadowColor = UIColor.blackColor().CGColor
+        label.layer.shadowOffset = CGSizeMake(0.1, 0.1)
+        label.layer.shadowOpacity = 1.0
+        label.layer.shadowRadius = 1.0
+    }
+    
     private class func colorCompenentFrom(string : String, atStartIndex start : Int, withLength length : Int) ->Float
     {
         let subString = ((string as NSString).substringWithRange(NSMakeRange(start, length))) as String
         let fullHex = length == 2 ? subString : (subString + subString)
         
-        var hexCompenent : Float = 0.0
+        var hexCompenent : UInt32 = 0
         let scanner = NSScanner(string: fullHex)
-        scanner.scanHexFloat(&hexCompenent)
+        scanner.scanHexInt(&hexCompenent)
         
         return Float(hexCompenent) / 255.0
     }
--- Flora Dummy/HomeVC.swift
+++ Flora Dummy/HomeVC.swift
@@ -10,33 +10,27 @@ import UIKit
 
 class HomeVC: UIViewController
 {
-    private let primaryColor : UIColor?
-    private let secondaryColor : UIColor?
-    private let backgroundColor : UIColor?
-    
+    //Elements on screen
     @IBOutlet var titleLabel : UILabel?
     @IBOutlet var subTitleLabel : UILabel?
     @IBOutlet var homeImageView : UIImageView?
     
     //Set the colors here for instant loading
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?)
-    {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        
-        let standardDefaults = NSUserDefaults.standardUserDefaults()
-    }
-
-    //Required in Swift.  Blank because we don't use it.
-    required init(coder aDecoder: NSCoder)
-    {
-        super.init(coder: aDecoder)
-    }
-    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //Directly set the colors since we don't need to later reference them at anytime
+        
+        let standardDefaults = NSUserDefaults.standardUserDefaults()
+        
+        titleLabel!.textColor = Definitions.colorWithHexString(standardDefaults.objectForKey("primaryColor") as String)
+        Definitions.outlineTextInLabel(titleLabel!)
+        
+        subTitleLabel!.textColor = Definitions.colorWithHexString(standardDefaults.objectForKey("secondaryColor") as String)
+        Definitions.outlineTextInLabel(subTitleLabel!)
+        
+        view.backgroundColor = Definitions.colorWithHexString(standardDefaults.objectForKey("backgroundColor") as String)
     }
 
 }
