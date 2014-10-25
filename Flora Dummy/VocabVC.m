a53d0d983e9291bf4631f85635ac8ca676e1a074
shaw65 <shaw65@purdue.edu>
2014-10-25 16:29:46 -0400

    adds button functionality

--- Flora Dummy/VocabVC.m
+++ Flora Dummy/VocabVC.m
@@ -39,9 +39,9 @@
     NSMutableArray *answers = [[NSMutableArray alloc]init];
     q1.answers = answers;
     [q1.answers addObject:@"Determined"];
-    [q1.answers addObject:@"Determined"];
-    [q1.answers addObject:@"Determined"];
-    [q1.answers addObject:@"Determined"];
+    [q1.answers addObject:@"Lazy"];
+    [q1.answers addObject:@"Satisfied"];
+    [q1.answers addObject:@"Content"];
 
     
     q2 = [[Question alloc] init];
@@ -84,18 +84,106 @@
 }
 
 - (IBAction)but2:(id)sender {
+    if ([[questions objectAtIndex:curQuestion] indexOfAnswer] == 1) {
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle: @""
+                                                       message: @"Correct!"
+                                                      delegate: self
+                                             cancelButtonTitle:@"Cancel"
+                                             otherButtonTitles:@"OK",nil];
+        
+        [alert setTag:1];
+        [alert show];
+        curQuestion+=1;
+    } else {
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle: @""
+                                                       message: @"Try Again"
+                                                      delegate: self
+                                             cancelButtonTitle:@"Cancel"
+                                             otherButtonTitles:@"OK",nil];
+        
+        [alert setTag:1];
+        [alert show];
+        
+    }
+
     
 }
 
 - (IBAction)but3:(id)sender {
+    if ([[questions objectAtIndex:curQuestion] indexOfAnswer] == 2) {
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle: @""
+                                                       message: @"Correct!"
+                                                      delegate: self
+                                             cancelButtonTitle:@"Cancel"
+                                             otherButtonTitles:@"OK",nil];
+        
+        [alert setTag:1];
+        [alert show];
+        curQuestion+=1;
+    } else {
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle: @""
+                                                       message: @"Try Again"
+                                                      delegate: self
+                                             cancelButtonTitle:@"Cancel"
+                                             otherButtonTitles:@"OK",nil];
+        
+        [alert setTag:1];
+        [alert show];
+        
+    }
+
     
 }
 
 - (IBAction)but4:(id)sender {
+    if ([[questions objectAtIndex:curQuestion] indexOfAnswer] == 3) {
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle: @""
+                                                       message: @"Correct!"
+                                                      delegate: self
+                                             cancelButtonTitle:@"Cancel"
+                                             otherButtonTitles:@"OK",nil];
+        
+        [alert setTag:1];
+        [alert show];
+        curQuestion+=1;
+    } else {
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle: @""
+                                                       message: @"Try Again"
+                                                      delegate: self
+                                             cancelButtonTitle:@"Cancel"
+                                             otherButtonTitles:@"OK",nil];
+        
+        [alert setTag:1];
+        [alert show];
+        
+    }
+
     
 }
 
 - (IBAction)but5:(id)sender {
+    if ([[questions objectAtIndex:curQuestion] indexOfAnswer] == 4) {
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle: @""
+                                                       message: @"Correct!"
+                                                      delegate: self
+                                             cancelButtonTitle:@"Cancel"
+                                             otherButtonTitles:@"OK",nil];
+        
+        [alert setTag:1];
+        [alert show];
+        curQuestion+=1;
+    } else {
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle: @""
+                                                       message: @"Try Again"
+                                                      delegate: self
+                                             cancelButtonTitle:@"Cancel"
+                                             otherButtonTitles:@"OK",nil];
+        
+        [alert setTag:1];
+        [alert show];
+        
+    }
+
     
 }
 
