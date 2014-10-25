2f3b9cf1ea85fc13c94f664a5ed8c248f85e7f5e
Michael Schloss <michaelschloss@pal-nat184-068-005.itap.purdue.edu>
2014-10-25 14:26:47 -0400

    Swift starts app

--- Flora Dummy/AppDelegate.swift
+++ Flora Dummy/AppDelegate.swift
@@ -11,11 +11,22 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UITabBarControllerDelegate
 {
+    var window: UIWindow?
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool
     {
         let userDefaults = ["gradeNumber":"Kindergarten", "primaryColor":"000000", "secondaryColor":"EBEBEB", "backgroundColor":"7EA7D8"]
         
         NSUserDefaults.standardUserDefaults().registerDefaults(userDefaults)
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        let storyBoard = UIStoryboard(name: "Main2", bundle: nil)
+        let tabBarController = storyBoard.instantiateInitialViewController() as UITabBarController
+        
+        window!.rootViewController = tabBarController
+        
+        window!.makeKeyAndVisible()
         
         return true
     }
