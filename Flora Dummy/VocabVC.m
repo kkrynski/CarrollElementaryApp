7e72fc1c730dd571fe0bd536b756aaa9baedb589
shaw65 <shaw65@purdue.edu>
2014-10-25 14:16:15 -0400

    questions

---
+++ Flora Dummy/VocabVC.m
@@ -0,0 +1,39 @@
+//
+//  VocabVC.m
+//  Flora Dummy
+//
+//  Created by Riley Shaw on 10/25/14.
+//  Copyright (c) 2014 SGSC. All rights reserved.
+//
+
+#import "VocabVC.h"
+
+@interface VocabVC ()
+
+@end
+
+@implementation VocabVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
