0ec6254bbf9a69c9d54c306679e0fc43f22faf5a
Michael Schloss <michaelschloss@pal-nat184-079-182.itap.purdue.edu>
2014-11-15 13:40:47 -0500

    Math Problem VC updates

--- Flora Dummy/MathProblemVC.swift
+++ Flora Dummy/MathProblemVC.swift
@@ -35,8 +35,6 @@ class MathProblemVC: PageVC
         
         let equation = MathProblemVC.convertStringIntoEquation(mathEquation!)
         
-        print(backgroundColor)
-        
         let equationView = UIView(frame: CGRectMake(0, 0, view.frame.size.width * 0.9, view.frame.size.height * 0.4))
         equationView.backgroundColor = Definitions.lighterColorForColor(backgroundColor!)
         Definitions.outlineView(equationView)
@@ -48,12 +46,18 @@ class MathProblemVC: PageVC
         equalsLabel.font = UIFont(name: "Marker Felt", size: 104)
         equalsLabel.sizeToFit()
         Definitions.outlineTextInLabel(equalsLabel)
-        equalsLabel.textColor = primaryColor!
+        equalsLabel.textColor = primaryColor
         equalsLabel.center = CGPointMake(equationView.frame.size.width * 0.7, equationView.frame.size.height/2.0)
         equationView.addSubview(equalsLabel)
         
-        createEquationLabel(equation[0], inView: equationView)
-        createAnswerSpace(equation[1], inView: equationView)
+        let equationViewView = UIView(frame: CGRectMake(8, 8, equalsLabel.frame.origin.x - 8, equationView.frame.size.height - 16))
+        equationView.addSubview(equationViewView)
+        
+        let answerView = UIView(frame: CGRectMake(equalsLabel.frame.origin.x + equalsLabel.frame.size.width + 8, 8, equationView.frame.size.width - 8 - (equalsLabel.frame.origin.x + equalsLabel.frame.size.width + 8), equationView.frame.size.height - 16))
+        equationView.addSubview(answerView)
+        
+        createEquationLabel(equation[0], inView: equationViewView)
+        createAnswerSpace(equation[1], inView: answerView)
     }
     
     func createEquationLabel(equation : String, inView equationView : UIView)
@@ -61,8 +65,33 @@ class MathProblemVC: PageVC
         
     }
     
-    func createAnswerSpace(answer : String, inView equationView : UIView)
+    func createAnswerSpace(answer : String, inView answerView : UIView)
     {
-        
+        switch answer
+        {
+        case "#w#":
+            let answerSpace = MathProblemTextField(frame: CGRectMake(0, 0, answerView.frame.size.width, answerView.frame.size.height * 0.5), andTextFieldType: "Whole Number")
+            answerSpace.center = CGPointMake(answerView.frame.size.width/2.0, answerView.frame.size.height/2.0)
+            answerSpace.backgroundColor = Definitions.lighterColorForColor(Definitions.lighterColorForColor(backgroundColor))
+            answerSpace.textColor = primaryColor
+            answerView.addSubview(answerSpace)
+            break
+            
+        case "#v#":
+            
+            break
+            
+        case "#fr#":
+            
+            break
+            
+        case "#d#":
+            
+            break
+            
+        default:
+            
+            break
+        }
     }
 }
