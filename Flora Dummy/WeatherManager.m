9e5123effd4384cde5b6786b5078a79dd8f8000a
Michael Schloss <michaelschloss@Michael-Schlosss-MacBook-Pro.local>
2014-11-22 00:34:26 -0500

    blaahhhhhh

--- Flora Dummy/WeatherManager.m
+++ Flora Dummy/WeatherManager.m
@@ -1,24 +1,9 @@
 //
 //  WeatherManager.m
-//  Weatherli
+//  FloraDummy
 //
-//  Created by Ahmed Eid on 5/14/12.
-//  Copyright (c) 2012 Ahmed Eid. All rights reserved.
-//  This file is part of Weatherli.
-//
-//  Weatherli is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  Weatherli is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with Weatherli.  If not, see <http://www.gnu.org/licenses/>.
-//
+//  Created by Michael Schloss on 11/21/14.
+//  Copyright (c) 2014 Michael Schloss. All rights reserved.
 
 #import "WeatherManager.h"
 
