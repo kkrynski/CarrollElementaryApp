2cf7baf5e6cbc984feed904153287dcbbbf3bf58
Zachary Nichols <Zach@pal-nat184-108-070.itap.purdue.edu>
2014-11-24 16:24:46 -0500

    Activity Creation with Editing
    Next Step is to create a content creation controller for every type of controller

--- Flora Dummy/ActivityCreationTVC.m
+++ Flora Dummy/ActivityCreationTVC.m
@@ -41,15 +41,17 @@
     NSString *path = [documentPath stringByAppendingPathComponent:@"myplist.plist"];
     NSLog(@"File path = %@", path);
 
+    pagesArray = [[NSMutableArray alloc] initWithObjects: nil];
+
     if ([NSDictionary dictionaryWithContentsOfFile:path])
     {
         NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:path];
         ClassConversions *cc = [[ClassConversions alloc] init];
         activity = [cc activityFromDictionary:dict];
+        pagesArray = [[NSMutableArray alloc] initWithArray: activity.pageArray];
 
     }
     
-    pagesArray = [[NSMutableArray alloc] initWithObjects: nil];
     currentIndex = 0;
     
     // Do any additional setup after loading the view, typically from a nib.
@@ -57,12 +59,15 @@
     
     UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
     UIBarButtonItem *saveButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSave target:self action:@selector(saveActivity:)];
-    self.navigationItem.rightBarButtonItems = @[addButton, saveButton];
+    self.navigationItem.rightBarButtonItems = @[addButton, self.editButtonItem, saveButton];
+    
     UIBarButtonItem *cancelButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancel)];
     self.navigationItem.leftBarButtonItem = cancelButton;
+    
     self.pageCreationVC = (PageCreationVC *)[[self.splitViewController.viewControllers lastObject] topViewController];
     self.pageCreationVC.delegate = self;
-    
+    self.pageCreationVC.page = (Page *)pagesArray.firstObject;
+
     // Create our font. Later we'll want to hook this up to the
     // rest of the app for easier change.
     font = [[UIFont alloc]init];
@@ -82,9 +87,22 @@
     }
     Page *newPage = [[Page alloc] init];
     
+    // Insert at front and move to end
+    //
+    // It's ugly, so if you can find another way to do it, please do.
+    [self.tableView beginUpdates];
+    
     [self.pagesArray insertObject:newPage atIndex:0];
     NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
-    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationNone];
+    [self.tableView moveRowAtIndexPath:indexPath toIndexPath:[NSIndexPath indexPathForRow:pagesArray.count-1 inSection:0]];
+    
+    [self.tableView endUpdates];
+   
+    
+    
+    
+    //[self.tableView reloadData];
     
     self.pageCreationVC.page = newPage;
     [self.pageCreationVC configureView];
@@ -144,14 +162,14 @@
     // cells that have long since left the screen. This saves memory and
     // keeps the app from crashing.
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
-                                       reuseIdentifier:@"Cell"];
+    if (!cell)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
     }
     
     // Update and format the title label, or the primary label in the cell.
     cell.textLabel.text = [(Page *)[pagesArray objectAtIndex:indexPath.row] pageVCType];
-    cell.textLabel.font = font;
+    //cell.textLabel.font = font;
     //cell.textLabel.textColor = primaryColor;
     //[self outlineTextInLabel:cell.textLabel];
     
@@ -180,28 +198,39 @@
     return YES;
 }
 
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete)
+    {
         [self.pagesArray removeObjectAtIndex:indexPath.row];
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        
+    } else if (editingStyle == UITableViewCellEditingStyleInsert)
+    {
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
     }
 }
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
 
-/*
+
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+    // Re-order page array
+    Page *pageToMove = [self.pagesArray objectAtIndex:fromIndexPath.row];
+    [self.pagesArray removeObjectAtIndex:fromIndexPath.row];
+    [self.pagesArray insertObject:pageToMove atIndex:toIndexPath.row];
+}
+
+
+
 // Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
     // Return NO if you do not want the item to be re-orderable.
     return YES;
 }
-*/
+
 
 /*
 #pragma mark - Navigation
@@ -237,7 +266,13 @@
 
     }
     
-    self.pageCreationVC.page = p;
+    [self.pageCreationVC setPage:p];
+    
+    [self.tableView beginUpdates];
+    [self.tableView reloadRowsAtIndexPaths:[NSArray arrayWithObjects:[NSIndexPath indexPathForRow:currentIndex inSection:0], nil] withRowAnimation:UITableViewRowAnimationNone];
+    [self.tableView endUpdates];
+    
+    //[self.tableView reloadData];
 }
 
 -(void)finishSavingActivity: (Activity *)a
