a709cba8dc1ef79e323b236554792f3fd6b56b1f
Michael Schloss <michaelschloss@Michael-Schlosss-MacBook-Pro.local>
2014-12-12 21:58:20 -0500

    updated README file for database integration.

---
+++ CESDatabase.swift
@@ -0,0 +1,19 @@
+//
+//  CESDatabase.swift
+//  FloraDummy
+//
+//  Created by Michael Schloss on 12/12/14.
+//  Copyright (c) 2014 SGSC. All rights reserved.
+//
+
+import Foundation
+
+protocol CESDatabase
+{
+    
+}
+
+private class PrivateCESDatabase : NSObject
+{
+    
+}
--- Flora Dummy/README.swift
+++ Flora Dummy/README.swift
@@ -8,7 +8,7 @@ Herein begins the Database Integration for the FloraDummy App
 
     This README file will detail the API as I create it.
 
-        *  This API will be built entirely in Objective-C, but will be fully compatible with all Swift files
+        *  This API will be built entirely in SWift, but will be fully compatible with all Objective-C files
         *  As of the current moment, all API calls will return an NSDictionary of data.
             *  The current layout of the NSDictionary is undecided right now,
                but I will update this file as I figure out the structure
@@ -18,12 +18,17 @@ Herein begins the Database Integration for the FloraDummy App
             *  You will be given constants to use as the dictionary's keys
 
 
-    There are certain rules that each activity must follow.  These rules will be constantly changing 
-      as I develop the API, so nothing here is final.  PLEASE NOTE: Zach's Activity Creation will follow
-      seperate rules later on in this README.
+    There are certain rules that each activity must follow, as listed below.  These rules will be constantly
+      changing as I develop the API, so nothing here is final.  PLEASE NOTE: Zach's Activity Creation will 
+      follow seperate rules later on in this README.
 
         *  Each activity will be responsible for keeping track of their current ActivityID.  This ID will be
-           used to update the activity's data
-        *  
+           used to update the activity's data/grades
+        *  You will be unable to interact with the Database beyond updating specific values and downloading 
+           any existing data for the activity.  This is to keep from accidental (or even purposeful)
+           overwrites
+        *  In the interest of data protection, you will only be given access to the Activity table.  You
+           will not be able to see or interact with the Subject, Teacher, Student, Class, Student_Class, or
+           Activity_Session tables
 
 */
