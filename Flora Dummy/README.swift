8db52efe3050168df1837aebea2a8b54c2cfd772
Michael Schloss <michaelschloss@pal-nat184-074-084.itap.purdue.edu>
2014-12-14 18:45:58 -0500

    updated README API descriptions

--- Flora Dummy/README.swift
+++ Flora Dummy/README.swift
@@ -7,20 +7,16 @@
 Herein begins the Database Integration for the FloraDummy App
 
     This README file will detail the API as I create it.
-
         *  This API will be built entirely in Swift, but will be fully compatible with all Objective-C files
-        *  As of the current moment, all API calls will return an NSDictionary of data.
-            *  The current layout of the NSDictionary is undecided right now,
-               but I will update this file as I figure out the structure
-        
-
+        *  As of the current moment, any download API (with the exception of activity loading) calls will 
+           post a notification when they're done
+            *  It is your responsibility to pull the data from the plist (this will be specified later)
         *  Uploading data will require an NSDictionary containing the necessary values
             *  You will be given constants to use as the dictionary's keys
 
 
     There are certain rules that each activity must follow, as listed below.  These rules will be constantly
       changing as I develop the API, so nothing here is final.
-
         *  Each activity will be responsible for keeping track of their current ActivityID.  This ID will be
            used to update the activity's data/grades
         *  You will be unable to interact with the Database beyond updating specific values and downloading 
@@ -33,7 +29,7 @@ Herein begins the Database Integration for the FloraDummy App
 ----[[ API ]]----
 
 
-    -- Database Manager creation--
+    -- Database Manager Creation --
 
         **  For Activity Creation, to implement the CESDatabase API, you must store a property with the type
             "id<CESCreationDatabase>"
@@ -68,7 +64,7 @@ Herein begins the Database Integration for the FloraDummy App
             to be returned the proper Database Manager
 
 
-    -- API Calls--
+    -- API Calls --
 
         Each Database Manager has different API calls, as listed below.  Please find the correct
         Database Manager class and follow its instructions
